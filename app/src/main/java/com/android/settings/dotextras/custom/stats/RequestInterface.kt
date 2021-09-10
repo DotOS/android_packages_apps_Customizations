54d1803185dfa4aed1a59669bc2361c68a4195de
IacobIonut01 <paulionut2003@gmail.com>
2021-09-10 21:35:46 +0300

    [5.2-Bugfix] Stats: Use "POST" method
    Signed-off-by: IacobIonut01 <paulionut2003@gmail.com>
    Change-Id: I2c54aeb373b99a4bf032c99e70a68552902d2bce

--- app/src/main/java/com/android/settings/dotextras/custom/stats/RequestInterface.kt
+++ app/src/main/java/com/android/settings/dotextras/custom/stats/RequestInterface.kt
@@ -3,10 +3,10 @@ package com.android.settings.dotextras.custom.stats
 import io.reactivex.Observable
 import retrofit2.http.Body
 import retrofit2.http.Headers
-import retrofit2.http.PUT
+import retrofit2.http.POST
 
 interface RequestInterface {
     @Headers("Content-Type: application/json")
-    @PUT("device/")
+    @POST("device/")
     fun operation(@Body request: ServerRequest?): Observable<ServerResponse?>?
 }
